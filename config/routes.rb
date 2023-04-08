c1dde9c0a237cf630bae3914829e0fda2d59cb3e
EC2 Default User <ec2-user@ip-172-31-24-207.ap-northeast-1.compute.internal>
2023-04-08 09:27:54 +0000

    Re

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
+
+  namespace :public do
+    get 'genres/show'
+  end
 # 顧客用
 # URL /customers/sign_in ...
-root to:"public/homes#top"
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+scope module: 'public' do
+    root 'homes#top'
+    resources :items, only: [:show, :index]
+    get 'about' => 'homes#about'
+   end
+
+
+namespace :public do
+
+  end
 
 # 管理者用
 # URL /admin/sign_in ...
