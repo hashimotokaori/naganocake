e314ad298aa0a0d680b0eb5ca2e513d68c2f5d79
EC2 Default User <ec2-user@ip-172-31-24-207.ap-northeast-1.compute.internal>
2023-05-23 12:05:16 +0000

    5.23

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@
     is_active: [['販売中', true], ['販売停止', false]]
   )
   item.image.attach(io: File.open(Rails.root.join('app/assets/images/cake6.jpg')), filename: 'cake.jpg')
+  
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
